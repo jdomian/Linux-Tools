8fbc33301c5050d68e45a6c82e6cfbce83a257d2
Jonathan Domian <jonathan.domian@gmail.com>
2022-11-30 07:29:19 -0600

    Update eeprom-firmware-update.sh

--- eeprom-firmware-update.sh
+++ eeprom-firmware-update.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get full-upgrade -y
+sudo apt-get rpi-eeprom-update -y
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get full-upgrade -y
+sudo apt-get rpi-eeprom-update -y
