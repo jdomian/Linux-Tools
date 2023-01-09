4491d6ab220a8a46cf33186d4e69fddc8d2d134d
Jonathan Domian <jonathan.domian@gmail.com>
2023-01-09 10:11:07 -0600

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -3,35 +3,46 @@
 # Run with:
 #sh macos-setup.sh
 
+# Show hidden files and folders
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Kayids Theme for Integrity computer
+# https://github.com/AmrMKayid/KayidmacOS/blob/master/kayid.zsh-theme
+# Add to /Users/jonathan.domian/.oh-my-zsh/themes/kayid.zsh-theme
+# Change ~/.zshrc ZSH_THEME="kayid"
 
+# Install and configure homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/jonathan.domian/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jonathan.domian/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "# Homebrew\nexport PATH=/opt/homebrew/bin:\$PATH" >> .zshrc
+source ~/.zshrc
 
+# Update homebrew
+brew update
 
 # Install git
 brew install git
 
 # Install NodeJS, NPM and NVM
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash
+brew install node=
+brew install nvm
+mkdir ~/.nvm
+echo "export NVM_DIR=~/.nvm\nsource \$(brew --prefix nvm)/nvm.sh" >> .zshrc
+source ~/.zshrc
 
-sudo apt-get install nodejs -y
+nvm install node
+nvm ls-remote
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-sudo npm install -g npm@latest -y
-sudo npm install --global gulp-cli -y
-
-# Install homebrew certificate installer for gulp
+# Install homebrew certificate installer for gulpJS
 brew install mkcert
 mkcert -install
 mkcert "locahost"
+
+# Install all gulpJS and firebase-tools dependencies
+npm install -g browser-sync del gulp gulp-babel gulp-clean-css gulp-cli gulp-concat gulp-csso gulp-install gulp-less gulp-load-plugins gulp-minify gulp-rename gulp-sass gulp-sourcemaps gulp-uglify sass express firebase-tools
