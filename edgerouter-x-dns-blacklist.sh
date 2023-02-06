71ae7e9955410c08373d7e9b1f777e775bcfa8c6
Jonathan Domian <jonathan.domian@gmail.com>
2023-02-06 14:00:58 -0600

    Create edgerouter-x-dns-blacklist.sh

---
+++ edgerouter-x-dns-blacklist.sh
@@ -0,0 +1,26 @@
+configure
+set system package repository blacklist components main
+set system package repository blacklist description 'Britannic blacklist debian stretch repository'
+set system package repository blacklist distribution stretch
+set system package repository blacklist url 'https://raw.githubusercontent.com/britannic/debian-repo/master/blacklist/public/'
+commit;save;exit
+
+sudo curl -L https://raw.githubusercontent.com/britannic/debian-repo/master/blacklist/public.key | sudo apt-key add -
+
+sudo apt-get update && sudo apt-get install -f edgeos-dnsmasq-blacklist
+
+apt --fix-broken install
+
+sudo apt-get clean cache
+delete system image
+
+curl -L -O https://raw.githubusercontent.com/britannic/blacklist/master/edgeos-dnsmasq-blacklist_1.2.4.7_mipsel.deb
+sudo dpkg -i edgeos-dnsmasq-blacklist_1.2.4.7_mipsel.deb
+
+sudo apt-get update && sudo apt-get install -f --only-upgrade edgeos-dnsmasq-blacklist
+
+sudo dpkg-reconfigure edgeos-dnsmasq-blacklist
+
+configure
+set service dns forwarding blacklist disabled false
+commit;save;exit
